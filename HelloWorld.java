83584385fcb5d7888250daee0dac93edd91a85b2
Your Name <you@example.com>
2017-02-22 17:25:19 -0600

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sindhu");
-		System.out.pringtln("Mahadev");
+		System.out.println("Mahadev");
 
 	}
 
